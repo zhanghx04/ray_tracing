1ef6344c5fd45484a236ea45d352f8ea48be8d2b
Haoxiang Zhang <zhanghx04@gmail.com>
2023-06-19 17:33:15 -0500

    Added a function for generate a real random number

--- src/common/utility.h
+++ src/common/utility.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 using std::shared_ptr;
 using std::make_shared;
@@ -20,6 +21,15 @@ inline double double_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    // Returns a random real in [0, 1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min, max)
+    return min + (max - min) * random_double();
+}
 
 
 // Common Headers
