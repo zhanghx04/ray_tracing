a3ca89fa989961931c5d1311aee4ec790aaf5295
Haoxiang Zhang <zhanghx04@gmail.com>
2023-06-27 16:38:32 -0500

    Added some comments

--- src/hittable.h
+++ src/hittable.h
@@ -11,7 +11,7 @@ struct hit_record {
     vec3 normal;
     shared_ptr<material> mat_ptr;
     double t;
-    double front_face;
+    double front_face;  // Whether the normal is face to outward
 
     inline void set_face_normal(const ray& r, const vec3& outward_normal) {
         front_face = dot(r.direction(), outward_normal) < 0;
