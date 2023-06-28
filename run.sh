c5f07e24e3cab59dfb98c7a2b6a838bfcd6ea169
zhanghx04 <zhanghx04@gmail.com>
2023-06-28 14:58:56 -0500

    Added the mac/linx version to the run file

--- run.sh
+++ run.sh
@@ -2,10 +2,24 @@
 
 echo "Start Running [Ray Tracing]"
 
-echo "Start to build the project"
-make -C build/
+UNAME=$(uname)
+
+echo "Removing current exist scene.ppm file"
+rm ./scene.ppm
+
+if [[ "$UNAME" == "Darwin" || "$UNAME" == "Linux" ]]; then
+    echo "[UNIX] Start to build the project"
+    make -C build/
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]]; then
+    echo "[WIN] Start to build the project"
+    make -C build/
+fi
 
 echo "Converting the result to [scene.ppm]"
-build/ray_tracing.exe > scene.ppm
+if [[ "$UNAME" == "Darwin" || "$UNAME" == "Linux" ]]; then
+    build/ray_tracing > scene.ppm
+elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]]; then
+    build/ray_tracing.exe > scene.ppm
+fi
 
 echo "Done!"
